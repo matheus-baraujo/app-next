cccfea2376f77f1e5eb31eb697818ce66533dd81
MBA <matheus-araujo1998@hotmail.com>
2022-11-09 12:55:06 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import config from "../config.json";
+import styled from "styled-components";
+import { CSSReset } from "../src/components/CSSReset";
+
+function HomePage() {
+  return (
+    <>
+      <CSSReset />
+      <div>Welcome to Next.js!</div>
+    </>
+  );
+}
+
+export default HomePage;
